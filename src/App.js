f80d7c2ea19fbd1024c4c40099863ab3df7491bd
cong98 <lethanhcongcnhn@gmail.com>
2022-01-20 18:08:04 +0700

    oke

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Dashboard from "./components/Dashboard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Layout/Header";
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+
 function App() {
   return (
-    <div className="App"><Header/>
-      <Dashboard/>
-    </div>
+      <Router>
+          <div className="App">
+              <Header/>
+              <Dashboard/>
+              <Route path="/addProject" component={AddProject}/>
+          </div>
+      </Router>
   );
 }
 
---
+++ src/components/Project/AddProject.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AddProject extends Component {
+    render() {
+        return (
+            <h1>hihihi</h1>
+        );
+    }
+}
+
+export default AddProject;
